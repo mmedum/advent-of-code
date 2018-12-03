2cba089892d4c9f33cd93220dcc1a3a8906cea63
Mark Medum Bundgaard <mmedum@gmail.com>
2018-12-03 14:11:25 +0100

    part 2 of problem 1

--- 2018/problem_1/main.py
+++ 2018/problem_1/main.py
@@ -1,15 +1,24 @@
 # https://adventofcode.com/2018/day/1
 # Starting with a frequency of zero, what is the resulting frequency
 # after all of the changes in frequency have been applied?
+# What is the first frequency your device reaches twice?
 
 
 def main():
     with open('input', 'r') as inputs:
+        seen_freq = {}
         freq = 0
-        for str_input in inputs.readlines():
-            freq = perform_freq_change(str_input, freq)
-
-        print(freq)
+        seen_freq[freq] = 1
+        searching_for_dup_freq = True
+        freq_inputs = inputs.readlines()
+        while searching_for_dup_freq:
+            for str_input in freq_inputs:
+                freq = perform_freq_change(str_input, freq)
+                if searching_for_dup_freq and freq in seen_freq:
+                    print(f'Already seen freq: {freq}')
+                    searching_for_dup_freq = False
+                seen_freq[freq] = 1
+            print(f'End freq: {freq}')
 
 
 def perform_freq_change(str_input, current_freq):
