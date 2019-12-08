d88b06df7d4a06f8a5e9975f8be8146fda49092a
Mark Medum Bundgaard <mmedum@gmail.com>
2019-12-08 23:30:00 +0100

    Adding solution to problem2 part2

--- 2019/problem2/main.go
+++ 2019/problem2/main.go
@@ -29,28 +29,45 @@ func main() {
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		commands := strings.Split(scanner.Text(), ",")
-		fmt.Println(commands)
+		memory := strings.Split(scanner.Text(), ",")
 
-		for i := 0; i < len(commands); i++ {
-			command, _ := strconv.Atoi(commands[i])
-
-			if command == 99 {
-				break
+		for noun := 0; noun <= 99; noun++ {
+			for verb := 0; verb <= 99; verb++ {
+				memoryCopy := make([]string, len(memory))
+				copy(memoryCopy, memory)
+				memoryCopy[1] = strconv.Itoa(noun)
+				memoryCopy[2] = strconv.Itoa(verb)
+				result := compute(memoryCopy)
+				if result == 19690720 {
+					fmt.Println(noun, verb)
+					fmt.Println(100*noun + verb)
+					os.Exit(0)
+				}
 			}
-
-			postion1, _ := strconv.Atoi(commands[i+1])
-			postion2, _ := strconv.Atoi(commands[i+2])
-			storePostion, _ := strconv.Atoi(commands[i+3])
-			value1, _ := strconv.Atoi(commands[postion1])
-			value2, _ := strconv.Atoi(commands[postion2])
-			if command == 1 {
-				commands[storePostion] = strconv.Itoa(value1 + value2)
-			} else {
-				commands[storePostion] = strconv.Itoa(value1 * value2)
-			}
-			i += 3
 		}
-		fmt.Println(commands[0])
 	}
+}
+
+func compute(memory []string) int {
+	for i := 0; i < len(memory); i++ {
+		operation, _ := strconv.Atoi(memory[i])
+
+		if operation == 99 {
+			result, _ := strconv.Atoi(memory[0])
+			return result
+		}
+
+		address1, _ := strconv.Atoi(memory[i+1])
+		address2, _ := strconv.Atoi(memory[i+2])
+		storeAddress, _ := strconv.Atoi(memory[i+3])
+		value1, _ := strconv.Atoi(memory[address1])
+		value2, _ := strconv.Atoi(memory[address2])
+		if operation == 1 {
+			memory[storeAddress] = strconv.Itoa(value1 + value2)
+		} else {
+			memory[storeAddress] = strconv.Itoa(value1 * value2)
+		}
+		i += 3
+	}
+	return 0
 }
