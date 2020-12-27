039eb4a96830ea206a60204100a351fe5c8f1f57
Mark Medum Bundgaard <mmedum@gmail.com>
2020-12-27 21:04:48 +0100

    Solution 2 on Day 1 2020

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -19,13 +19,16 @@ func main() {
 
 	sliceData := strings.Split(string(fileBytes), "\n")
 
-	for i := 0; i < len(sliceData)-1; i++ {
-		for j := i + 1; j < len(sliceData); j++ {
-			a, _ := strconv.Atoi(sliceData[i])
-			b, _ := strconv.Atoi(sliceData[j])
-			if a+b == 2020 {
-				fmt.Println(a * b)
-				return
+	for i := 0; i < len(sliceData)-2; i++ {
+		for j := i + 1; j < len(sliceData)-1; j++ {
+			for x := j + 1; x < len(sliceData); x++ {
+				a, _ := strconv.Atoi(sliceData[i])
+				b, _ := strconv.Atoi(sliceData[j])
+				c, _ := strconv.Atoi(sliceData[x])
+				if a+b+c == 2020 {
+					fmt.Println(a * b * c)
+					return
+				}
 			}
 		}
 	}
